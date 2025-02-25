718c1d3d9f0f21a3c71d572b3fdd8aa02e1120b1
SildCave <szymekanix@gmail.com>
2025-02-25 20:48:58 +0100

    1

---
+++ main.py
@@ -0,0 +1,35 @@
+imie = "john"
+print(imie)
+
+imie = "kamil"
+print(imie)
+
+imie = "can't"
+print(imie)
+
+imie = 'can\'t'
+print(imie)
+
+dom = "dach\nkuchnia"
+print(dom)
+
+ukosnik_n = "\\n"
+print(ukosnik_n)
+
+ukosnik_n = '\n'
+
+kox1 = '*'
+kox2 = '*'
+print("kox1: " + kox1)
+print("kox2: " + kox1)
+
+kox1 += '*'
+kox2 = kox2 + '*'
+
+# '*' + '*' = '**'
+print("kox1: " + kox1)
+print("kox2: " + kox1)
+
+kox = '*' * 10
+print(kox)
+
